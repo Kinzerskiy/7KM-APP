465f3360bc6b76f83ed59df290951dd6adb6189d
Kinzerskiy <kinzzersky@gmail.com>
2023-01-14 17:41:28 +0200

    Initial Commit

---
+++ RusikApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RusikApp
+//
+//  Created by ANTON on 14.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
